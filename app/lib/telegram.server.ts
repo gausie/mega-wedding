505d769dc33265adaccf09fbd9312aaf6e5a46cb
Sam Gaus <sam@gaus.co.uk>
2022-06-13 11:10:17 +0100

    Telegram bot key scret

--- app/lib/telegram.server.ts
+++ app/lib/telegram.server.ts
@@ -1,8 +1,6 @@
 import { Telegraf } from "telegraf";
 
-export const bot = new Telegraf(
-  "5574906501:AAEV6ztjU39JFKj97UumwCqYdZkHXlcYyro"
-);
+export const bot = new Telegraf(process.env.TELEGRAM_BOT_KEY || "");
 
 export const sendTelegramMessage = (message: string) => {
   bot.telegram.sendMessage(-676164038, message);
